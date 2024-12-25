58a77573e55d42d584bc63894173af6377a4fca5
Mario Araos <marioscloud@duck.com>
2024-12-25 17:25:08 +0000

    Add photos to the repository

--- main.tf
+++ main.tf
@@ -14,9 +14,8 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
 
   tags = {
-    Name = "main"
+    Name = "VPC_AWS_01"
   }
 }
