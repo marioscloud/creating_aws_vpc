dc16d23300761b5d3ee127bc260adbb1d814d6a6
Mario Araos <marioscloud@duck.com>
2024-12-25 16:51:48 +0000

    It was added the VPC resource to main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,12 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
